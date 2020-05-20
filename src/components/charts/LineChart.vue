28c918834bb5bafbfda2c58b07dd55165682a77d
viktorkozachok21 <viktorkozachok21@gmail.com>
2020-05-20 20:58:22 +0300

    Create a test chart.

---
+++ src/components/charts/LineChart.vue
@@ -0,0 +1,28 @@
+<script>
+import { Line } from "vue-chartjs"
+
+export default {
+    name: "LineChart",
+    extends: Line,
+    props: {
+        dataSet: Array,
+    },
+    mounted() {
+        this.renderChart(this.setupChart(), {
+            responsive: true
+        })
+    },
+    methods: {
+        setupChart() {
+            return {
+                labels: this.dataSet.map(item => item.time),
+                datasets: [{
+                    label: "Vehicle",
+                    data: this.dataSet.map(item => item.speed),
+                    borderColor: "#638062"
+                }]
+            }
+        }
+    }
+}
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,30 +2,28 @@
 <div class="main_layout">
     <ReadFile @gotFile="loadDataFromFile" />
     <button type="button" @click="getVehicles">Go</button>
-    <button type="button" @click="getTimesteps">Time</button>
+    <LineChart v-if="this.listOfVehicles" :data-set="listOfVehicles[999]"/>
 </div>
 </template>
 
 <script>
 import ReadFile from "@/components/ReadFile.vue"
+import LineChart from "@/components/charts/LineChart.vue"
 
 export default {
     name: "MainLayout",
     data: () => ({
         trafficData: null,
-        listOfTimesteps: [],
         listOfVehicles: null
     }),
     components: {
-        ReadFile
+        ReadFile,
+        LineChart
     },
     methods: {
         loadDataFromFile(value) {
             if (value["fcd-export"]["timestep"]) this.trafficData = value["fcd-export"]["timestep"]
             console.log("got it")
-        },
-        getTimesteps() {
-            this.listOfTimesteps = this.trafficData.map(item => +item._time)
         },
         groupBy(array, key) {
             return array.reduce((result, currentValue) => {
@@ -42,20 +40,22 @@ export default {
                     "id": vehicle._id,
                     "lane": vehicle._lane,
                     "speed": vehicle._speed,
-                    "type": vehicle._type
+                    "type": vehicle._type,
+                    "time": item._time
                 }))
                 else this.listOfVehicles.push({
                     "id": item.vehicle._id,
                     "lane": item.vehicle._lane,
                     "speed": item.vehicle._speed,
-                    "type": item.vehicle._type
+                    "type": item.vehicle._type,
+                    "time": item._time
                 })
             })
             this.listOfVehicles = this.listOfVehicles.reduce((result, currentValue) => {
                 (result[currentValue["id"]] = result[currentValue["id"]] || []).push(currentValue)
                 return result
             }, {})
-            console.log(this.listOfVehicles)
+            console.log("done")
         }
     }
 }
