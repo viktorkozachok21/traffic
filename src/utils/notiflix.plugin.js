7c93b9ee6a61ba0c5053b0bd490480ac99d3c717
viktorkozachok21 <viktorkozachok21@gmail.com>
2020-06-21 19:43:37 +0300

    Deployed to firebase hosting

--- src/utils/notiflix.plugin.js
+++ src/utils/notiflix.plugin.js
@@ -2,7 +2,7 @@ import Notiflix from 'notiflix'
 
 export default {
   install(Vue) {
-    Vue.prototype.$notiflixSetLoading = () => Notiflix.Loading.Circle('Loading...')
+    Vue.prototype.$notiflixSetLoading = () => Notiflix.Loading.Circle('Обробка даних..')
     Vue.prototype.$notiflixRemoveLoading = () => Notiflix.Loading.Remove()
   }
 }
